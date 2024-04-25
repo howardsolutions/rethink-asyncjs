9f23abfdafaabed3d76c973ed6ce22ae9a07d5ed
thinhphungdev <phungthinh0946519356@gmail.com>
2024-04-25 17:56:55 +0700

    ex3: promise solution

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -1,27 +1,50 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: 'The first text',
+    file2: 'The middle text',
+    file3: 'The last text',
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log('Requesting: ' + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+  return new Promise((resolve, reject) => {
+    fakeAjax(file, function (responseText) {
+      resolve(responseText);
+    });
+    // 2 way of doing thing => fakeAjax(file, resolve);
+  });
 }
 
-// request all files at once in "parallel"
-// ???
+const p1 = getFile('file1');
+const p2 = getFile('file2');
+const p3 = getFile('file3');
+
+p1.then(function (responseText1) {
+  output(responseText1);
+
+  return p2;
+})
+  .then(function (responseText2) {
+    output(responseText2);
+    return p3;
+  })
+  .then(function (responseText3) {
+    output(responseText3);
+    output('COMPLETED!');
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
