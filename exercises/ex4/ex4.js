82ab68c84aa78db1c9ec4a46a2e010dedbfb320e
thinhphungdev <phungthinh0946519356@gmail.com>
2024-04-26 07:04:59 +0700

    ex4: ultility functianality of arbitrary length promise

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -1,29 +1,29 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: 'The first text',
+    file2: 'The middle text',
+    file3: 'The last text',
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log('Requesting: ' + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 // The old-n-busted callback way
 
 function getFile(file) {
-	return new Promise(function(resolve){
-		fakeAjax(file,resolve);
-	});
+  return new Promise(function (resolve) {
+    fakeAjax(file, resolve);
+  });
 }
 
 // Request all files at once in
@@ -34,3 +34,13 @@ function getFile(file) {
 // is done.
 
 // ???
+['file1', 'file2', 'file3']
+  .map(getFile)
+  .reduce(function (acc, currentPromise) {
+    return acc
+      .then(function () {
+        return currentPromise;
+      })
+      .then(output);
+  }, Promise.resolve())
+  .then(output('Completed!'));
